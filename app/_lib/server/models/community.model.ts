f65f0874335435480ee7b4f9ea239eb9db698bb0
acolpitts <adam@colpitts.dev>
2023-12-21 14:48:28 -0400

    wip: mongoose models

---
+++ app/_lib/server/models/community.model.ts
@@ -0,0 +1,58 @@
+import type { Document } from 'mongoose'
+import { model, Schema } from 'mongoose'
+import type { MembershipDocument } from './membership.model'
+
+export interface CommunityInput {
+  title: string
+  description?: string
+  isPublic?: boolean
+  theme?: object
+}
+
+export interface CommunityDocument extends CommunityInput, Document {
+  memberships?: MembershipDocument[]
+  updatedAt: Date
+  createdAt: Date
+}
+
+const CommunitySchema = new Schema<CommunityDocument>(
+  {
+    title: {
+      type: String,
+      trim: true,
+      validate: {
+        validator(v: string) {
+          return /^(?!\s*$).+/.test(v)
+        },
+        message: 'Please add a valid title.',
+      },
+      required: [true, 'Title is required.'],
+    },
+    description: {
+      type: String,
+    },
+    isPublic: {
+      type: Boolean,
+      default: true,
+    },
+    memberships: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Membership',
+      },
+    ],
+    theme: Object,
+  },
+  {
+    timestamps: true, // to create updatedAt and createdAt
+  },
+)
+
+/**
+ * Community Model
+ * @alpha
+ * ----
+ * A Community is a group of people who share a common interest.
+ *
+ */
+export const Community = model<CommunityDocument>('Community', CommunitySchema)
