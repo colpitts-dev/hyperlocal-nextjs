4375440c2737d7191a63b1ccdefc0bd2726d474a
acolpitts <adam@colpitts.dev>
2023-12-23 18:43:56 -0400

    feat: add basic admin dashboard

---
+++ app/(private)/members/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Members Dashboard</h1>
+    </main>
+  )
+}
